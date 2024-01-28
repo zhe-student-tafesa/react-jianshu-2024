c1ceb3267b3d628491e02424aa0b02e147c595a8
frankzhang2022 <frank.zhang@pixelforce.com.au>
2024-01-28 20:34:02 +1000

    1. add redux-devtools-extension;

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
-import reducer from './reducer.js'
+import { createStore, compose, applyMiddleware } from "redux";
+import reducer from './reducer.js';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeEnhancers());
 
 export default store;
