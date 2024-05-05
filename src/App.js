d3997dbfffbf23e60d480866ed3b25f245ce21df
frankzhang2022 <frank.zhang@pixelforce.com.au>
2024-05-05 13:26:13 +1000

    1. add Router;

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import { GlobalStyleIcon } from './statics/iconfont/iconfont.js';
 import Header from "./common/header/index.js";
 import { Provider } from "react-redux";
 import store from './store/index.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <div>
         <GlobalStyle />
         <GlobalStyleIcon />
-        <Header></Header>
-      </Provider>
+        <Provider store={store}>
+          <div>
+            <Header></Header>
+            <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<div>home</div>} />
+                <Route path='/detail' element={<div>detail</div>} />
+                <Route path='/login' element={<div>login</div>} />
+                <Route path='/write' element={<div>write</div>} />
+              </Routes>
+            </BrowserRouter>
 
+          </div>
+        </Provider>
+      </div>
     );
   }
 }
